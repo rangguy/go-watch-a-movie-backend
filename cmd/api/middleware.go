a93d68555e7cac5deb6fbe0503234bb33bc1c02e
rangguy <ranggadwi100@gmail.com>
2024-09-15 00:43:20 +0700

    update url frontend

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://go-watch-a-movie.vercel.app/")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
 			return
@@ -19,7 +19,7 @@ func (app *application) enableCORS(h http.Handler) http.Handler {
 
 func (app *application) authRequired(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, _, err  := app.auth.GetTokenFromHeaderAndVerify(w, r)
+		_, _, err := app.auth.GetTokenFromHeaderAndVerify(w, r)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
